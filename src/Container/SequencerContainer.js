d0c18b57118d76c86b9ec0218d895ecf2a14318c
David Eastcroft <davideastcroft95@gmail.com>
2020-08-09 12:00:07 +0100

    Adds sequencer buttons work in progress

--- src/Container/SequencerContainer.js
+++ src/Container/SequencerContainer.js
@@ -1,52 +1,78 @@
 import React, {Component} from 'react';
 
-const sequencePatterns = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
-
 class SequencerContainer extends Component{
 
   constructor(props){
     super(props);
     this.state = {
       bpm: 120,
-      patterns: sequencePatterns,
+      pattern: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
       playing: false
     }
-
+    this.activateButton1 = this.activateButton1.bind(this);
+    this.activateButton2 = this.activateButton2.bind(this);
+    this.activateButton3 = this.activateButton3.bind(this);
+    this.activateButton4 = this.activateButton4.bind(this);
   }
 
   play() {
     this.setState(() => ({
       playing: true
     }))
-
     this.interval = setInterval(() => {
-      this.state.patterns.map((pattern, index) => (pattern === 1 ? this.props.playSound() : null))
+      this.state.pattern.map((button, index) => (button === 1 ? this.props.playSound() : null))
     }, (60000 / this.state.bpm))
   }
 
   pause() {
     this.setState(() => ({
-      playing: false,
-      step: 0
+      playing: false
     }))
-
     clearInterval(this.interval)
+  }
+
+  activateButton1(){
+    if (this.state.pattern[0] === 0){
+      this.state.pattern[0] = 1;
+    }
+    else this.state.pattern[0] = 0;
+  }
+
+  activateButton2(){
+    if (this.state.pattern[1] === 0){
+      this.state.pattern[1] = 1;
+    }
+    else this.state.pattern[1] = 0;
+  }
+
+  activateButton3(){
+    if (this.state.pattern[2] === 0){
+      this.state.pattern[2] = 1;
+    }
+    else this.state.pattern[2] = 0;
+  }
+
+  activateButton4(){
+    if (this.state.pattern[3] === 0){
+      this.state.pattern[3] = 1;
+    }
+    else this.state.pattern[3] = 0;
   }
 
 
   render(){
     return(
       <div>
-      <button
-              type="button"
-              className={this.state.playing ? 'active' : ''}
-              onClick={() => {
+      <button type="button" onClick={() => {
                 if (this.state.playing) this.pause()
                 else this.play()
-              }}
-            >
+              }}>
               Start
-            </button>
+      </button>
+      <button onClick={this.activateButton1}>{this.state.pattern[0]}</button>
+      <button onClick={this.activateButton2}>{this.state.pattern[1]}</button>
+      <button onClick={this.activateButton3}>{this.state.pattern[2]}</button>
+      <button onClick={this.activateButton4}>{this.state.pattern[3]}</button>
       </div>
     )
   }
